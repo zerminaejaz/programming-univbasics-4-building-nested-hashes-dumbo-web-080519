169c33634a665dc422d78e3463646d6e94d69662
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 18:57:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,6 +26,7 @@ def monopoly_with_third_tier
           :reading_railroad => {},
           :pennsylvania_railroad => {},
           :mortgage_value => {},
+          :b_and_o_railroad => {}.
           
         },
         :rent_in_dollars => 
