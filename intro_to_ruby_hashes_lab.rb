2f7ac073aea84a51e97ae729bdadb7c69a85e04b
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 19:00:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,22 +23,22 @@ def monopoly_with_third_tier
         :pieces => 4,
         :names => 
         {
-          :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
-      :pennsylvania_railroad=>
-      {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
+            :reading_railroad=>
+        {
+          "mortgage_value"=>"$100"
+        },
+        :pennsylvania_railroad=>
+        {
+          "mortgage_value"=>"$200"
+        },
+        :b_and_o_railroad=>
+        {
+          "mortgage_value"=>"$400"
+        },
+        :shortline=>
+        {
+          "mortgage_value"=>"$800"
+        }
         },
         :rent_in_dollars => 
         {
