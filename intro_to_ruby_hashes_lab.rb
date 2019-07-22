7cc89d637c90ceae39703ec25be1f22cd98003d5
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 18:55:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,8 +9,9 @@ def monopoly_with_second_tier
     :railroads => 
     {
       :pieces => 4,
-      :rent_in_dollars => {},
-      :names => {}
+      :names => {},
+      :rent_in_dollars => {}
+    
     }
   }
 end
