fe4fdc0b68d003fecdd6b38fb860263c50680726
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-22 18:50:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,12 @@ end
 def monopoly_with_second_tier
   monopoly = {
     :railroads => 
-    {
-      :pieces => 4
-      :rent_in_dollars => {}
-      :names => {}
-    }
+    {}
+  }
+  monopoly[:railraods] = {
+    :pieces = 4
+    :rent_in_dollars => {}
+    :names => {}
   }
 end
 
